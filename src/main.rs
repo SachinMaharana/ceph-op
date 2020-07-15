ccb3b13ed902dabce01504b96d5fa4205cc9a542
sachin maharana <sachin.nicky@gmail.com>
2020-07-16 01:53:39 +0530

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use ceph::ceph as ceph_helpers;
 use ceph::ceph::*;
 use ceph::cmd::{osd_pool_get, PoolOption};
 use ceph::rados::{Struct_rados_cluster_stat_t, Struct_rados_pool_stat_t};
@@ -32,10 +33,28 @@ fn get_pool_size(handle: &Rados, pool: &str) -> Result<u32, String> {
 
 fn main() {
     let mut pool_usage: Vec<PoolInfo> = Vec::new();
+
     let c = connect_to_ceph("admin", "/opt/ceph.conf");
+
     match c {
         Ok(c) => {
-            let pools = c.rados_pools().map_err(|e| e.to_string()).unwrap();
+            // println!("Ceph Health Status: {}", c.ceph_health_string().unwrap());
+
+            println!("{}", c.ceph_health_string().unwrap_or("".to_string()));
+
+            let rados_version = ceph_helpers::rados_libversion();
+            println!("Librados version: {:?}", rados_version);
+
+            println!("Getting cluster fsid");
+            let fsid = c.rados_fsid();
+            println!("rados_cluster_fsid {:?}", fsid);
+
+            let cluster_stat = c.rados_stat_cluster().unwrap();
+            println!("Cluster stat: {:?}", cluster_stat);
+
+            // println!("{}", c.ceph_status(&["health"]).unwrap());
+
+            let pools = c.rados_pools().unwrap();
             println!("pools: {:?}", pools);
 
             for p in pools {
@@ -43,6 +62,8 @@ fn main() {
                 let i = c.get_rados_ioctx(&p).map_err(|e| e.to_string()).unwrap();
                 println!("Running stat against the pool");
                 let pool_stats = i.rados_stat_pool().map_err(|e| e.to_string()).unwrap();
+                println!("Number of Objects: {}", pool_stats.num_objects);
+                println!("Number of RD: {}", pool_stats.num_rd);
                 pool_usage.push(PoolInfo {
                     name: p.clone(),
                     usage: pool_stats,
